67dfbe4ef720f646fa2b06315d39dbe233553570
unknown <nitinprodduturi15@gmail.com>
2023-05-31 12:31:33 +0530

    Requirements added and Installation done

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r",encoding='utf-8') as f:
+    long_description = f.read()
+
+__version__='0.0.0'
+
+REPO_NAME="Text-Summarizer-Project"
+AUTHOR_USER_NAME="nitinprodduturi"
+SRC_REPO="textSummarizer"
+AUTHOR_EMAIL="nitinprodduturi15@gmail.com"
+
+
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="Text Summarizer NLP Project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": "https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)
+
